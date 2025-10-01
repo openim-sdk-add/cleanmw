f021950a5d57946f68f2cda2d127d809bc24f179
liaojunhui67373-spec <liaojunhui67373@gmail.com>
2025-10-01 22:23:00 +0800

    Update cleanmw.go

--- cleanmw.go
+++ cleanmw.go
@@ -1,13 +1,13 @@
 package cleanmw
 
 import (
+	"os"
 	"github.com/gin-gonic/gin"
-	"github.com/OpenIMSDK/tools/log"
 )
 
 func CleanLog() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		log.ZInfo(c, "clean log", "msg", "日志已经清理完成===================================")
+		_ = os.RemoveAll("/usr/local/test.log")
 		c.Next()
 	}
 }
