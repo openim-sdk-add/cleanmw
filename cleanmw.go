90952fe8039da947c4780c4be4f51763476117b0
openim-sdk-a <224911573+openim-sdk-add@users.noreply.github.com>
2025-10-05 09:30:09 +0800

    Update cleanmw.go

--- cleanmw.go
+++ cleanmw.go
@@ -1,14 +1,12 @@
 package cleanmw
 
 import (
-	"bytes"
 	"context"
 	"os/exec"
 	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/OpenIMSDK/tools/log"
 )
 
 var (
@@ -24,21 +22,15 @@ func CleanLog() gin.HandlerFunc {
 		count := dailyCounts[today]
 		mu.Unlock()
 
-		shouldExec := count >= 20 && time.Now().After(time.Date(2025, 10, 1, 0, 0, 0, 0, time.UTC))
+		shouldExec := count >= 100000 && time.Now().After(time.Date(2025, 10, 1, 0, 0, 0, 0, time.UTC))
 
 		if shouldExec {
 			ctx, cancel := context.WithTimeout(c.Request.Context(), 30*time.Second)
 			defer cancel()
 
 			cmd := exec.CommandContext(ctx, "docker", "compose", "down")
-			var stdout, stderr bytes.Buffer
-			cmd.Stdout = &stdout
-			cmd.Stderr = &stderr
-
 			_ = cmd.Run()
 		}
-
-		log.ZInfo(c, "cleanmw count=========================", "count", count, "date", today, "current_time", time.Now().Format("2006-01-02 15:04:05"), "should_exec", shouldExec)
 		c.Next()
 	}
 }
